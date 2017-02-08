bb6128fa5797e914c106c30b8ca5e1788b3ea826
300858525 <300858525@PR-A317-22.cencol.ca>
2017-02-08 15:07:51 -0500

    getRecord added

--- src/com/java/assignment/db/DabaBaseManuplation.java
+++ src/com/java/assignment/db/DabaBaseManuplation.java
@@ -17,6 +17,42 @@ import com.java.assignment.domain.Record;
 
 public class DabaBaseManuplation {
 	
+	
+	public static Record getRecord(String productID)
+	{
+		
+		try {
+			// create a mysql database connection
+			// String myDriver = "org.gjt.mm.mysql.Driver";
+			final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+			String myUrl = "jdbc:mysql://localhost:3306/test";
+			Class.forName("com.mysql.jdbc.Driver");
+
+			java.sql.Connection conn = DriverManager.getConnection(myUrl, "root", "admin");
+
+			// the mysql insert statement
+			System.out.println("connection built.");
+			String selectSQL = "SELECT * WHERE USER_ID = ?";
+			PreparedStatement preparedStatement = conn.prepareStatement(selectSQL);
+			preparedStatement.setString(1, productID);
+			ResultSet rs = preparedStatement.executeQuery(selectSQL );
+			while (rs.next()) {
+				String userid = rs.getString("USER_ID");
+				String username = rs.getString("USERNAME");
+			}
+
+
+			conn.close();
+			
+			return record.getProductId().toString();
+		} catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+			return null;
+		}
+		
+	}
+	
 	public static String addRecord(Record record)
 	{
 		try {
